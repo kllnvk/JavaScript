0c3ba7b5d9b0ab3ed864e629e82d65b06b126b5c
knovik <knovik@elinext.com>
2025-10-16 20:51:11 +0300

    Refactor HW-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -12,10 +12,10 @@ let dice2 = Math.ceil(Math.random() * 6);
 let isWinningDouble = ((dice1 === dice2) && (dice1+dice2) > 3) ? true : false;
 
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
-}
+// console.log('Первый бросок: ' + dice1)
+// console.log('Второй бросок: ' + dice2)
+// if (isWinningDouble) {
+//   console.log('Выигрышный дубль!')
+// } else {
+//   console.log('Не выигрышный дубль.')
+// }
