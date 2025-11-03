35899d42d298b49b251a4cba944fbfa5856141f5
Kiryl Novik <kllnvk13@gmail.com>
2025-11-03 15:05:02 +0300

    HW-4

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,24 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(array1, array2) {
+  let newArray = [];
+  if (array1.length >= array2.length) {
+    for (let i = 0; i < array2.length; i++) {
+      if (includesElement(array1, array2[i]) === true) {
+        newArray.push(array2[i]);
+      }
+    }
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (includesElement(array2, array1[i]) === true) {
+        newArray.push(array1[i]);
+      }
+    }
+  }
+  return newArray;
+}
+
+console.log(findCommonElements([1, 2, 3], [2, 3, 4]));
+console.log(findCommonElements([1, 2, 3, 7], [2, 3, 4, 5, 6, 7]));
+console.log(findCommonElements([1, 7, 2, 3, 5, 3, 7], [2, 3, 3, 4, 7]));
--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,12 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let newStr = '';
+    for (let i = 0; i < str.length; i++) {
+        newStr += str[i] + str[i];
+    }
+    return newStr;
+}
+
+console.log(doubleEachCharacter('hello'));
