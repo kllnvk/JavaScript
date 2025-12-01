1af3f831ada107d33ee37d8b464750b7a7165689
Kiryl Novik <kllnvk13@gmail.com>
2025-12-01 22:16:01 +0300

    HW-8-ref

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -10,4 +10,4 @@ function capitalizeWords(str) {
   return str.split(' ').map(w => w[0].toUpperCase() + w.slice(1).toLowerCase()).join(' ');
 }
 
-console.log(capitalizeWords("hello woASdld from javascript"))
+// console.log(capitalizeWords("hello woASdld from javascript"))
--- lesson-08/02.js
+++ lesson-08/02.js
@@ -7,8 +7,8 @@ function isNumeric(str) {
   return str === ' ' || isNaN(Number(str.trim())) ? false : true;
 }
 
-console.log(isNumeric("123")) // Ожидаемый результат: true
-console.log(isNumeric("12.3")) // Ожидаемый результат: true
-console.log(isNumeric("123abc")) // Ожидаемый результат: false
-console.log(isNumeric("abc")) // Ожидаемый результат: false
-console.log(isNumeric(" ")) // Ожидаемый результат: false
+// console.log(isNumeric("123")) // Ожидаемый результат: true
+// console.log(isNumeric("12.3")) // Ожидаемый результат: true
+// console.log(isNumeric("123abc")) // Ожидаемый результат: false
+// console.log(isNumeric("abc")) // Ожидаемый результат: false
+// console.log(isNumeric(" ")) // Ожидаемый результат: false
